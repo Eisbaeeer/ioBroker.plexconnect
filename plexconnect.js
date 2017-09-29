3ecccb4f7161888cc7b521f4d1cdf584cede59e5
Lars Weimar <Eisbaeeer@gmail.com>
2017-09-29 17:16:31 +0200

    working on code

--- plexconnect.js
+++ plexconnect.js
@@ -15,73 +15,26 @@ var express = require('express'),
     request = require('request'),
     multer  = require('multer');
 var app = express();
-//var isPlaying = true;
 var upload = multer({ dest: '/tmp/' });
-
-//app.post('/', upload.single('thumb'), function (req, res, next) {
-//var payload = JSON.parse(req.body.payload);
-//console.log('Got webhook for', payload.event);
-
 
 app.post('/', upload.single('thumb'), function (req, res, next) {
     var payload = JSON.parse(req.body.payload);
     adapter.log.info('getting payload');
     adapter.log.info(payload.Server.title);
+
+// Account
+    adapter.setState (adapter.namespace + '.' + 'account.id', {val: payload.Account.id, ack: true});
+    adapter.setState (adapter.namespace + '.' + 'account.thumb', {val: payload.Account.thumb, ack: true});
+    adapter.setState (adapter.namespace + '.' + 'account.title', {val: payload.Account.title, ack: true});
     
+// Event
+    adapter.setState (adapter.namespace + '.' + 'event.Name', {val: payload.event, ack: true});
+
 // MUSIK 
-//  if ( payload.event == "media.play" && payload.Metadata.type == "track") {
-     adapter.setState (adapter.namespace + '.' + 'who', {val: payload.Metadata.grandparentTitle, ack: true});
-//  setState("javascript.0.Plex.wer",payload.Metadata.grandparentTitle); 
-     adapter.setState (adapter.namespace + '.' + 'Title', {val: payload.Metadata.parentTitle, ack: true});
- //   setState("javascript.0.Plex.Titel"/*Plex.Titel*/,payload.Metadata.parentTitle); 
-//    setState("javascript.0.Plex.parentTitel"/*Plex.parentTitel*/,payload.Metadata.title);
-//    setState("javascript.0.Plex.Rating"/*Plex.Rating*/,payload.Metadata.ratingCount.toString()); 
-//    setState("javascript.0.Plex.media"/*Plex.media*/,payload.Metadata.librarySectionType); 
-      adapter.setState (adapter.namespace + '.' + 'logo', {val: payload.Metadata.thumb, ack: true});
-//    var logo=url+':32400'+payload.Metadata.thumb;
-//    setState("javascript.0.Plex.logo"/*Plex.logo*/,logo); 
-//    setState("javascript.0.Plex.media"/*Plex.media*/,"Musik");
-//}
-           
- //*****************************
- // VIDEOS & HomeVIDEOS
-//   if ( payload.event == "media.play" && payload.Metadata.type == "movie") {
-//   setState("javascript.0.Plex.wer",payload.Metadata.studio); 
-//   setState("javascript.0.Plex.Titel"/*Plex.Titel*/,payload.Metadata.title); 
-//    setState("javascript.0.Plex.Rating"/*Plex.Rating*/,payload.Metadata.rating.toString()); 
-//    var logo2=url+':32400'+payload.Metadata.thumb;  
-//    setState("javascript.0.Plex.logo"/*Plex.logo*/,logo2); 
-//    var logo3=url+':32400'+payload.Metadata.art;
-//    setState("javascript.0.Plex.parentlogo"/*Plex.parentlogo*/,logo3); 
-//    setState("javascript.0.Plex.media"/*Plex.media*/,"Video");
-//    } 
-        
+    adapter.setState (adapter.namespace + '.' + 'metadata.grandparentTitle', {val: payload.Metadata.grandparentTitle, ack: true});
+    adapter.setState (adapter.namespace + '.' + 'metadata.parentTitle', {val: payload.Metadata.parentTitle, ack: true});
+    adapter.setState (adapter.namespace + '.' + 'metadata.thumb', {val: payload.Metadata.thumb, ack: true});
 
-//*****************************
-  // Bilder wieder auf null
-//   if ( payload.event == "media.stop") {
-
-//   setState("javascript.0.Plex.wer",""); 
-//   setState("javascript.0.Plex.Titel"/*Plex.Titel*/,""); 
-//   setState("javascript.0.Plex.logo"/*Plex.logo*/,'http://192.168.1.184:8082/vis.0/main/img/firework-music.jpg'); 
-//   setState("javascript.0.Plex.parentTitel"/*Plex.parentTitel*/,"");
-//   setState("javascript.0.Plex.Rating"/*Plex.Rating*/,""); 
-//   setState("javascript.0.Plex.media"/*Plex.media*/,"");
-//   setState("javascript.0.Plex.parentlogo"/*Plex.parentlogo*/,""); 
-//   }
-//  if (payload.event == "media.play" || payload.event == "media.resume") {
-//    isPlaying = true;
-//  } else if (payload.event == "media.pause" || payload.event == "media.stop") {
-//    isPlaying = false;
-//  }
-
-//  res.sendStatus(200);
-
-//var options = {
-//  headers: {
-//  }
-//}
-//}
 });
 
 adapter.on('ready', function () {
@@ -89,7 +42,6 @@ adapter.on('ready', function () {
   
     // starting express server
     var server = app.listen(adapter.config.port, function () {
-//    var server = app.listen(12000, function () {
     var port = server.address().port;
     adapter.log.info('Server listening on port:' + port);
 });
